fb6c80e08aa82f2671cdef8f9c995536bd5b190a
Vladyslav Levkovich <levkovich.vlad.2004@gmail.com>
2024-10-05 19:10:41 +0300

    http requests flask

--- app/main.py
+++ app/main.py
@@ -1,14 +1,39 @@
-from flask import Flask
+from flask import Flask, request, send_file, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def welcome():
+    return "Привіт! Це другий урок по фреймовку Flask"
 
 @app.route("/users/<int:user_id>")
 def get_username(user_id):
     return f'Hello {user_id}'
+
+@app.route("/login", methods=['POST'])
+def enter_user_data():
+    if request.method == 'POST':
+        name = request.form['name']
+        print(request.form)
+        return {'message': name}
+
+
+@app.route("/register", methods=['POST'])
+def user_register():
+    if request.method == 'POST':
+        data = request.json
+        email = data['email']
+        password = data['password']
+        first_name = data['first_name']
+
+        if len(password) < 8:
+            return {'message': 'Твій пароль надто короткий'}
+        return {'email': email, 'password': password, 'first_name': first_name}
+
+
+@app.route("/redirect")
+def redirect_route():
+    return redirect('https://www.google.com/')
 
 if __name__ == '__main__':
     app.run(debug=True)
